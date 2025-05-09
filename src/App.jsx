b54d43a11f929b6ca5a9606912e1eef7599115c7
Hajar Elyazri <hajarelyazri10@gmail.com>
2025-05-09 15:53:53 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'; // Ensure this file exists
+import Home from './Home';
+
+
+function App() {
+ 
+
+  return (
+    <div>
+   <Home  />
+   </div> 
+  )
+}
+
+export default App;
